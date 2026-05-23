8eea2a20527c00a2c2a581250d8f179f84ea23ed
Kent Dybvig <18339284+dybvig@users.noreply.github.com>
2026-05-23 19:36:49 +0200

    another possible CI fix
    Signed-off-by: Kent Dybvig <18339284+dybvig@users.noreply.github.com>

--- tests-e2e/src/tests/bugs/compiler.bugs.e2e.test.ts
+++ tests-e2e/src/tests/bugs/compiler.bugs.e2e.test.ts
@@ -187,7 +187,7 @@ describe('[Bugs] Compiler', () => {
                 testcase: 'should compile when contract includes multiple modules',
                 file: 'main.compact',
                 output: {
-                    stderr: '',
+                    stderr: 'Compiling 0 circuits:',
                     stdout: compilerDefaultOutput(),
                     exitCode: ExitCodes.Success,
                 },
