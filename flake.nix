8e64a04963dee5fa0dc74d689a1bb1f022c277b9
JosephDenman <joseph.denman@iohk.io>
2026-06-30 14:38:16 -0500

    Bump 'zkir-v3' to 'rc.2' + secp
    Signed-off-by: JosephDenman <joseph.denman@iohk.io>

--- flake.nix
+++ flake.nix
@@ -50,12 +50,12 @@
     };
     zkir-v3 = {
       # zkir-v3 binary for v3 IR format
-      url = "github:midnightntwrk/midnight-ledger/tkerber/zkir-v3-tests"; # zkir-v3
+      url = "github:midnightntwrk/midnight-ledger/JosephDenman/zkir-v3-rc2-rc3-patch"; # zkir-v3
       inputs.zkir.follows = "zkir";
     };
     zkir-v3-wasm = {
       # zkir-v3-wasm for test-center v3 support
-      url = "github:midnightntwrk/midnight-ledger/tkerber/zkir-v3-tests";
+      url = "github:midnightntwrk/midnight-ledger/JosephDenman/zkir-v3-rc2-rc3-patch";
       inputs.zkir.follows = "zkir";
     };
     n2c.url = "github:nlewo/nix2container";
