8b1f4011f3ae7516af0f2c31166e3f1db10fb286
pataei <paris.ataei@gmail.com>
2026-07-24 14:54:32 -0400

    allow unused import
    Signed-off-by: pataei <paris.ataei@gmail.com>

--- tools/compact/tests/test_self_scenarios.rs
+++ tools/compact/tests/test_self_scenarios.rs
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(unused_imports)]
+
 use crate::common::{
     COMPACT_VERSION, PREVIOUS_COMPACT_VERSION, download_to_temp, run_downloaded_binary,
 };
